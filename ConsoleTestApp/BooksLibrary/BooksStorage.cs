2c905de84814fb9aaed245255a129156578387eb
mbashynskyi <mbashynskyi@gmail.com>
2026-04-13 19:17:11 +0300

    Cleanup.

--- ConsoleTestApp/BooksLibrary/BooksStorage.cs
+++ ConsoleTestApp/BooksLibrary/BooksStorage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BooksLibrary.Interfaces;
 
 namespace BooksLibrary;
 
--- ConsoleTestApp/BooksLibrary/Interfaces/IBooksStorage.cs
+++ ConsoleTestApp/BooksLibrary/Interfaces/IBooksStorage.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace BooksLibrary;
+namespace BooksLibrary.Interfaces;
 
 public enum SearchPart
 {
