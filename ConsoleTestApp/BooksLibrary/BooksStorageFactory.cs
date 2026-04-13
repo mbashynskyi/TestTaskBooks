f54cd1a8acdd9b363bd678b8df2a45db81fe17c0
mbashynskyi <mbashynskyi@gmail.com>
2026-04-13 19:36:07 +0300

    Added factory to get BooksStorage instance as IBooksStorage interface.

---
+++ ConsoleTestApp/BooksLibrary/BooksStorageFactory.cs
@@ -0,0 +1,9 @@
+﻿using BooksLibrary.Interfaces;
+using System;
+
+namespace BooksLibrary;
+
+public class BooksStorageFactory
+{
+    public IBooksStorage GetBooksStorage() => new BooksStorage();
+}
